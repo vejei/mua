0006fc4a1d87c09a3154c7b17975a360e7b9bce1
zeleven <chengsnem@163.com>
2018-01-03 17:38:52 +0800

    Create BaseFragment.java

---
+++ app/src/main/java/io/github/zeleven/mua/BaseFragment.java
@@ -0,0 +1,21 @@
+package io.github.zeleven.mua;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        initView();
+        return view;
+    }
+
+    public abstract int getLayoutId();
+
+    public abstract void initView();
+}
