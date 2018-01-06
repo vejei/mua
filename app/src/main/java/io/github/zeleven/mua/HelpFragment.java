0d2060a618ab73a0f1bfc59bd11465a892a0a8cb
zeleven <chengsnem@163.com>
2018-01-06 22:16:27 +0800

    update

--- app/src/main/java/io/github/zeleven/mua/HelpFragment.java
+++ app/src/main/java/io/github/zeleven/mua/HelpFragment.java
@@ -1,18 +1,18 @@
 package io.github.zeleven.mua;
 
-import butterknife.BindString;
-
 public class HelpFragment extends BaseFragment {
-    @BindString(R.string.drawer_item_help)
-    String title;
     @Override
     public int getLayoutId() {
         return R.layout.fragment_help;
     }
 
     @Override
+    public int getTitleResId() {
+        return R.string.drawer_item_help;
+    }
+
+    @Override
     public void initView() {
-        fragmentToolbarTitle = title;
         super.initView();
     }
 }
