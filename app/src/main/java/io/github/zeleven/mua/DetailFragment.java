345b0711761b1f4b45e55d4a3ec62831e933f9ba
zeleven <chengsnem@163.com>
2018-01-08 19:05:30 +0800

    Create DetailFragment.java

---
+++ app/src/main/java/io/github/zeleven/mua/DetailFragment.java
@@ -0,0 +1,8 @@
+package io.github.zeleven.mua;
+
+public class DetailFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_detail;
+    }
+}
