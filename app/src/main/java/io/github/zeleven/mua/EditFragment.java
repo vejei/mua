b242e5f76a1b07bca0b381dc94468bbf0a167f58
zeleven <chengsnem@163.com>
2018-01-08 19:02:38 +0800

    Create EditFragment.java

---
+++ app/src/main/java/io/github/zeleven/mua/EditFragment.java
@@ -0,0 +1,13 @@
+package io.github.zeleven.mua;
+
+public class EditFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_edit;
+    }
+
+    @Override
+    public void initView() {
+        super.initView();
+    }
+}
