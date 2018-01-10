042efdc502e25b1a4b8587e7a39931107f85c8b3
zeleven <chengsnem@163.com>
2018-01-10 17:26:06 +0800

    Create EventBus event class

---
+++ app/src/main/java/io/github/zeleven/mua/ContentChangedEvent.java
@@ -0,0 +1,9 @@
+package io.github.zeleven.mua;
+
+public class ContentChangedEvent {
+    String content;
+
+    public ContentChangedEvent(String content) {
+        this.content = content;
+    }
+}
---
+++ app/src/main/java/io/github/zeleven/mua/TitleChangedEvent.java
@@ -0,0 +1,8 @@
+package io.github.zeleven.mua;
+
+public class TitleChangedEvent {
+    String title;
+    public TitleChangedEvent(String title) {
+        this.title = title;
+    }
+}
