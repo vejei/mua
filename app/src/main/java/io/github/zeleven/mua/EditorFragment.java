4166f658a9145bafebeb99cd0851a7a076cb7587
zeleven <chengsnem@163.com>
2018-01-10 19:18:53 +0800

    setting viewpager

--- app/src/main/java/io/github/zeleven/mua/EditorFragment.java
+++ app/src/main/java/io/github/zeleven/mua/EditorFragment.java
@@ -1,8 +1,50 @@
 package io.github.zeleven.mua;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import butterknife.BindView;
+
 public class EditorFragment extends BaseFragment {
+    @BindView(R.id.editor_viewpager) ViewPager editorViewPager;
+
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.fragment_editor;
     }
+
+    @Override
+    public void initView() {
+        toolbarTitle = "";
+        super.initView();
+        final ScreenSlidePagerAdapter adapter = new ScreenSlidePagerAdapter(
+                context.getSupportFragmentManager());
+        editorViewPager.setAdapter(adapter);
+        editorViewPager.setCurrentItem(0);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment;
+            if (position == 0) {
+                fragment = new EditFragment();
+            } else {
+                fragment = new MarkdownPreviewFragment();
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
 }
