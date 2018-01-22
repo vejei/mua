25cab639ff802fdb945827209b0cc5571963db84
zeleven <chengsnem@163.com>
2018-01-22 21:30:52 +0800

    Create constants manager class

---
+++ app/src/main/java/io/github/zeleven/mua/Constants.java
@@ -0,0 +1,13 @@
+package io.github.zeleven.mua;
+
+public class Constants {
+    public static final String MY_EMAIL = "";
+    public static final String MY_GITHUB = "http://github.com/zeleven";
+    public static final String MY_ZHIHU = "https://www.zhihu.com/people/zheng-zheng-shu-36";
+    public static final String PROJECT_PAGE_URL = "";
+
+    public static final String BUNDLE_KEY_FROM_FILE = "FROM_FILE";
+    public static final String BUNDLE_KEY_SAVED = "SAVED";
+    public static final String BUNDLE_KEY_FILE_NAME = "FILE_NAME";
+    public static final String BUNDLE_KEY_FILE_PATH = "FILE_PATH";
+}
