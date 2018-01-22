af64910f4cf5e6b36d33ccfdc066b51a82958467
zeleven <chengsnem@163.com>
2018-01-22 21:33:44 +0800

    update

--- app/src/main/java/io/github/zeleven/mua/SaveFileTask.java
+++ app/src/main/java/io/github/zeleven/mua/SaveFileTask.java
@@ -2,35 +2,46 @@ package io.github.zeleven.mua;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.widget.Toast;
 
-public class SaveFileTask extends AsyncTask<Void, Void, Void> {
+public class SaveFileTask extends AsyncTask<Void, Void, Boolean> {
     private Context context;
+    private String filePath;
     private String fileName;
     private String content;
+    private Response response;
 
-    public SaveFileTask(Context context, String fileName, String content) {
+    public SaveFileTask(Context context, String filePath, String fileName, String content,
+                        Response response) {
         this.context = context;
+        this.filePath = filePath;
         this.fileName = fileName;
         this.content = content;
+        this.response = response;
     }
 
     @Override
-    protected Void doInBackground(Void... params) {
+    protected Boolean doInBackground(Void... params) {
+        boolean result;
         if (TextUtils.isEmpty(fileName)) {
-            toastMessage(R.string.message_title_empty);
+            toastMessage(R.string.toast_message_file_name_can_not_empty);
+            result = false;
         } else {
-            String filePath = Environment.getExternalStorageDirectory().toString()
-                    + "/" + "Mua" + "/" + fileName + ".md";
-            int resId = FileUtils.saveFile(filePath, fileName, content);
-            if (resId != 0) {
-                toastMessage(resId);
+            result = FileUtils.saveFile(filePath, content);
+            if (result) {
+                toastMessage(R.string.message_saved);
+            } else {
+                toastMessage(R.string.message_file_exist);
             }
         }
-        return null;
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        response.taskFinish(aBoolean);
     }
 
     public void toastMessage(final int resId) {
@@ -41,5 +52,9 @@ public class SaveFileTask extends AsyncTask<Void, Void, Void> {
                 Toast.makeText(context, resId, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    public interface Response {
+        void taskFinish(Boolean result);
     }
 }
