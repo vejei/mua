d1407c05e5d4cf08d387d8a12a103410c358867a
zeleven <chengsnem@163.com>
2018-01-22 22:12:26 +0800

    update

--- app/src/main/java/io/github/zeleven/mua/SettingsFragment.java
+++ app/src/main/java/io/github/zeleven/mua/SettingsFragment.java
@@ -1,8 +1,12 @@
 package io.github.zeleven.mua;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
 
 import butterknife.BindString;
 
@@ -29,18 +33,46 @@ public class SettingsFragment extends BaseFragment {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             addPreferencesFromResource(R.xml.preferences);
 
+            final AppCompatActivity context = (AppCompatActivity) getContext();
+
+            // open AboutFragment on preference click
             Preference drawerHeaderPreference = findPreference("drawer_header");
-            drawerHeaderPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            drawerHeaderPreference.setOnPreferenceClickListener(
+                    new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
                     return true;
                 }
             });
 
+            // get version name and version code
+            String versionName = "";
+            int versionCode = 1;
+            try {
+                PackageInfo packageInfo = context.getPackageManager().getPackageInfo(
+                        context.getPackageName(), 0);
+                versionName = packageInfo.versionName;
+                versionCode = packageInfo.versionCode;
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e(getClass().getName(), e.getMessage());
+                e.printStackTrace();
+            }
+            // set version name and version code for preference item's summary
+            Preference versionPreference = findPreference("check_update");
+            String appVersionText = getResources().getString(R.string.app_version_text);
+            versionPreference.setSummary(appVersionText + " " + versionName
+                    + " ( " + versionCode + " ) ");
+
+            // open AboutFragment on preference click
             Preference aboutPreference = findPreference("about");
-            aboutPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            aboutPreference.setOnPreferenceClickListener(
+                    new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
+                    context.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, new AboutFragment())
+                            .addToBackStack(null)
+                            .commit();
                     return true;
                 }
             });
