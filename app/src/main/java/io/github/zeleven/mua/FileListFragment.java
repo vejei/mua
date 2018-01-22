7295e9ae1ca2a0e658f263e5ac59ded476fe4d93
zeleven <chengsnem@163.com>
2018-01-22 20:38:44 +0800

    update

--- app/src/main/java/io/github/zeleven/mua/FileListFragment.java
+++ app/src/main/java/io/github/zeleven/mua/FileListFragment.java
@@ -2,51 +2,36 @@ package io.github.zeleven.mua;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-
-import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindString;
 import butterknife.BindView;
-import butterknife.OnClick;
 
 public class FileListFragment extends BaseFragment {
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
     @BindView(R.id.file_list) RecyclerView fileListRecyclerView;
-    @BindView(R.id.fab_menu_container) FrameLayout fabMenuContainer;
-    @BindView(R.id.fab_menu) FloatingActionsMenu fabMenu;
     @BindView(R.id.create_markdown_btn) FloatingActionButton createMarkdownBtn;
     @BindView(R.id.navigation_view) NavigationView navigationView;
 
     @BindString(R.string.app_name) String appName;
-    String rootDir;
 
     @Override
     public int getLayoutId() {
@@ -58,17 +43,11 @@ public class FileListFragment extends BaseFragment {
         toolbarTitle = appName;
         setDisplayHomeAsUpEnabled = false;
         super.initView();
-        setFabMenu();
+        setFab();
         setHasOptionsMenu(true);
         setDrawerToggle();
         setNavigationViewItemListener();
-//        readAppFiles();
-        rootDir = Environment.getExternalStorageDirectory().getAbsoluteFile() + "/" + appName;
-        ArrayList<Object> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(new MarkdownFile());
-        }
-        FilesAdapter adapter = new FilesAdapter(list);
+        FilesAdapter adapter = new FilesAdapter(readAppFiles());
         fileListRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         fileListRecyclerView.setItemAnimator(new DefaultItemAnimator());
         fileListRecyclerView.addItemDecoration(new DividerItemDecoration(context,
@@ -81,19 +60,18 @@ public class FileListFragment extends BaseFragment {
      *  If the button menu expanded, change background color for menu container.
      *  Otherwise, change background to transparent.
      */
-    public void setFabMenu() {
-        fabMenuContainer.setVisibility(View.VISIBLE);
-        fabMenu.setOnFloatingActionsMenuUpdateListener(
-                new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
+    public void setFab() {
+        final Fragment fragment = new EditorFragment();
+        Bundle args = new Bundle();
+        args.putBoolean("FROM_FILE", false);
+        fragment.setArguments(args);
+        createMarkdownBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMenuExpanded() {
-                fabMenuContainer.setBackgroundColor(ContextCompat.getColor(context,
-                        R.color.semi_transparent_white));
-            }
-
-            @Override
-            public void onMenuCollapsed() {
-                fabMenuContainer.setBackgroundColor(Color.TRANSPARENT);
+            public void onClick(View v) {
+                context.getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
     }
@@ -131,6 +109,7 @@ public class FileListFragment extends BaseFragment {
                         selectedFragment = new HelpFragment();
                         break;
                     case R.id.settings:
+                        selectedFragment = new SettingsFragment();
                         break;
                 }
                 navigationView.setCheckedItem(item.getItemId());
@@ -147,7 +126,8 @@ public class FileListFragment extends BaseFragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.toolbar_menu, menu);
+        menu.clear();
+        inflater.inflate(R.menu.filelist_fragment_menu, menu);
         MenuItem searchItem = menu.findItem(R.id.search);
         SearchManager searchManager = (SearchManager) context.getSystemService
                 (Context.SEARCH_SERVICE);
@@ -157,83 +137,16 @@ public class FileListFragment extends BaseFragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        switch (item.getItemId()) {
-            case R.id.sort:
-                // open sort options dialog
-                builder.setTitle(R.string.toolbar_menu_sort);
-                builder.setSingleChoiceItems(R.array.sort_options, 0,
-                        new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0:
-                                Toast.makeText(context, "" + 0, Toast.LENGTH_LONG).show();
-                                break;
-                            case 1:
-                                Toast.makeText(context, "" + 1, Toast.LENGTH_LONG).show();
-                                break;
-                            case 2:
-                                Toast.makeText(context, "" + 2, Toast.LENGTH_LONG).show();
-                                break;
-                        }
-                    }
-                });
-                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.show();
-                break;
-            case R.id.show_options:
-                // open show options settings dialog
-                final ArrayList selectedItems = new ArrayList();
-                builder.setTitle(R.string.toolbar_menu_show_option);
-                boolean[] checkedItems = {true, true};
-                builder.setMultiChoiceItems(R.array.show_options, checkedItems,
-                        new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                        if (isChecked) {
-                            selectedItems.add(which);
-                        } else if (selectedItems.contains(which)) {
-                            selectedItems.remove(which);
-                        }
-                    }
-                });
-                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // do something here
-                        Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                builder.show();
-                break;
-        }
         return super.onOptionsItemSelected(item);
     }
 
     private List<File> readAppFiles() {
-        String filesPath = Environment.getExternalStorageDirectory().getAbsoluteFile()
-                + "/" + appName;
-        return Arrays.asList(new File(filesPath).listFiles());
-    }
-
-    @OnClick(R.id.create_markdown_btn)
-    public void createMarkdownFile() {
-        fabMenu.collapse();
-        context.getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new EditorFragment())
-                .addToBackStack(null)
-                .commit();
+        String filesPath = Environment.getExternalStorageDirectory().toString()
+                + "/" + appName + "/";
+        List<File> fileList = FileUtils.listFiles(filesPath);
+        if (fileList == null) {
+            FileUtils.createFolder(filesPath);
+        }
+        return fileList;
     }
 }
