2553ccaf8ca4129c2ff96de2440b5f28bc533358
zeleven <chengsnem@163.com>
2018-01-22 20:34:40 +0800

    update

--- app/src/main/java/io/github/zeleven/mua/EditFragment.java
+++ app/src/main/java/io/github/zeleven/mua/EditFragment.java
@@ -1,19 +1,51 @@
 package io.github.zeleven.mua;
 
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
+import java.io.File;
+
+import butterknife.BindString;
 import butterknife.BindView;
 
-public class EditFragment extends BaseFragment {
-    @BindView(R.id.title_input) EditText titleInput;
+public class EditFragment extends BaseEditorFragment implements View.OnClickListener {
     @BindView(R.id.content_input) EditText contentInput;
+    @BindString(R.string.app_name) String appName;
+
+    @BindView(R.id.heading) ImageButton headingBtn;
+    @BindView(R.id.bold) ImageButton boldBtn;
+    @BindView(R.id.italic) ImageButton italicBtn;
+    @BindView(R.id.code) ImageButton blockCodeBtn;
+    @BindView(R.id.quote) ImageButton quoteBtn;
+    @BindView(R.id.list_number) ImageButton listNumberBtn;
+    @BindView(R.id.list_bullet) ImageButton listBulletBtn;
+    @BindView(R.id.link) ImageButton linkBtn;
+    @BindView(R.id.image) ImageButton imageBtn;
+
+    @BindString(R.string.editor_dialog_item_local_image) String localImage;
+    @BindString(R.string.editor_dialog_item_internet_image) String internetImage;
+
+    private String rootPath;
+    private EditorAction editorAction;
+
+    public EditFragment() {
+        this.editorAction = new EditorAction();
+    }
 
     @Override
     public int getLayoutId() {
@@ -22,37 +54,19 @@ public class EditFragment extends BaseFragment {
 
     @Override
     public void initView() {
-        toolbarTitle = "";
         super.initView();
-        addListenerForInput();
+        rootPath = Environment.getExternalStorageDirectory().toString() + "/" + appName + "/";
+        if (fileContent != null) {
+            contentInput.setText(fileContent);
+        }
         setHasOptionsMenu(true);
-    }
-
-    public void addListenerForInput() {
-        titleInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                EventBus.getDefault().post(new TitleChangedEvent(s.toString()));
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-        contentInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                EventBus.getDefault().post(new ContentChangedEvent(s.toString()));
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
+        if (editorAction == null) {
+            editorAction = new EditorAction();
+        }
+        editorAction.setContext(context);
+        editorAction.setEditText(contentInput);
+        contentInput.requestFocus();
+        setOnClickListener();
     }
 
     @Override
@@ -62,13 +76,254 @@ public class EditFragment extends BaseFragment {
     }
 
     @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuItem item = menu.findItem(R.id.rename);
+        if (saved) {
+            item.setEnabled(true);
+        } else {
+            item.setEnabled(false);
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.undo:
+                editorAction.undo();
                 break;
             case R.id.redo:
+                editorAction.redo();
+                break;
+            case R.id.save:
+                if (!saved) {
+                    AlertDialog.Builder saveDialog = new AlertDialog.Builder(context);
+                    saveDialog.setTitle(R.string.editor_dialog_title_save_file);
+
+                    LayoutInflater inflater = context.getLayoutInflater();
+                    final View view = inflater.inflate(R.layout.dialog_save_file, null);
+                    final EditText fileNameET = view.findViewById(R.id.file_name);
+
+                    saveDialog.setView(view);
+                    saveDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    saveDialog.setPositiveButton("保存", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            fileName = fileNameET.getText().toString();
+                            filePath = rootPath + fileName + ".md";
+//                            Toast.makeText(context, fileName, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(context, filePath, Toast.LENGTH_SHORT).show();
+                            new SaveFileTask(context, filePath, fileName,
+                                    contentInput.getText().toString(), new SaveFileTask.Response() {
+                                @Override
+                                public void taskFinish(Boolean result) {
+                                    saved = result;
+                                }
+                            }).execute();
+                        }
+                    });
+
+                    saveDialog.show();
+                } else {
+                    editorAction.update(filePath);
+                }
+                break;
+            case R.id.rename:
+                if (saved) {
+                    AlertDialog.Builder renameDialog = new AlertDialog.Builder(context);
+                    renameDialog.setTitle("重命名");
+
+                    LayoutInflater inflater = context.getLayoutInflater();
+                    View view = inflater.inflate(R.layout.dialog_save_file, null);
+                    final EditText fileNameET = view.findViewById(R.id.file_name);
+                    fileNameET.setText(fileName);
+                    fileNameET.setSelection(fileName.length());
+
+                    renameDialog.setView(view);
+                    renameDialog.setNegativeButton(R.string.cancel,
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    renameDialog.setPositiveButton("重命名", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            fileName = fileNameET.getText().toString();
+                            FileUtils.renameFile(new File(filePath),
+                                    new File(rootPath + fileName + ".md"));
+                            filePath = rootPath + fileName + ".md";
+                        }
+                    });
+                    renameDialog.show();
+                }
+                break;
+            case R.id.clear_all:
+                editorAction.clearAll();
+                break;
+            case R.id.md_docs:
+                editorAction.checkDocs();
+                break;
+            case R.id.statistics:
+                editorAction.statistics();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.heading:
+                editorAction.heading();
+                break;
+            case R.id.bold:
+                editorAction.bold();
+                break;
+            case R.id.italic:
+                editorAction.italic();
+                break;
+            case R.id.code:
+                editorAction.insertCode();
+                break;
+            case R.id.quote:
+                editorAction.quote();
+                break;
+            case R.id.list_number:
+                editorAction.orderedList();
+                break;
+            case R.id.list_bullet:
+                editorAction.unorderedList();
+                break;
+            case R.id.link:
+                editorAction.insertLink();
+                break;
+            case R.id.image:
+                // open dialog to insert image
+                final CharSequence[] items = {localImage, internetImage};
+                AlertDialog.Builder optionsDialog = new AlertDialog.Builder(context);
+                optionsDialog.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (items[which].equals(items[0])) {
+                            // select local image
+                            Intent intent = new Intent();
+                            intent.setType("image/*");
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            startActivityForResult(intent, 1);
+                        } else if (items[which].equals(items[1])) {
+                            dialog.cancel();
+
+                            // insert image from internet
+                            AlertDialog.Builder inputDialog = new AlertDialog.Builder(context);
+                            inputDialog.setTitle(R.string.editor_dialog_title_insert_image);
+                            LayoutInflater inflater = context.getLayoutInflater();
+                            final View dialogView = inflater.inflate(R.layout.dialog_insert_image, null);
+                            inputDialog.setView(dialogView);
+                            inputDialog.setNegativeButton(R.string.cancel,
+                                    new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                            inputDialog.setPositiveButton(R.string.editor_dialog_btn_insert,
+                                    new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    EditText imageDisplayText = dialogView.findViewById(
+                                            R.id.image_display_text);
+                                    EditText imageUri = dialogView.findViewById(R.id.image_uri);
+                                    editorAction.insertImage(imageDisplayText.getText().toString(),
+                                            imageUri.getText().toString());
+                                }
+                            });
+                            inputDialog.show();
+                        }
+                    }
+                });
+                optionsDialog.show();
+                break;
+        }
+    }
+
+    public void setOnClickListener() {
+        headingBtn.setOnClickListener(this);
+        boldBtn.setOnClickListener(this);
+        italicBtn.setOnClickListener(this);
+        blockCodeBtn.setOnClickListener(this);
+        quoteBtn.setOnClickListener(this);
+        listNumberBtn.setOnClickListener(this);
+        listBulletBtn.setOnClickListener(this);
+        linkBtn.setOnClickListener(this);
+        imageBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == context.RESULT_OK) {
+                final Uri selectedImage = data.getData();
+                AlertDialog.Builder inputDialog = new AlertDialog.Builder(context);
+                inputDialog.setTitle(R.string.editor_dialog_title_insert_image);
+
+                LayoutInflater inflater = context.getLayoutInflater();
+                View view = inflater.inflate(R.layout.dialog_insert_image, null);
+                final EditText imageDisplayText = view.findViewById(R.id.image_display_text);
+                EditText imageUri = view.findViewById(R.id.image_uri);
+                imageUri.setText(selectedImage.getPath());
+
+                inputDialog.setView(view);
+                inputDialog.setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                inputDialog.setPositiveButton(R.string.editor_dialog_btn_insert,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String displayText = imageDisplayText.getText().toString();
+                                editorAction.insertImage(displayText, selectedImage.getPath());
+                            }
+                        });
+                inputDialog.show();
+            } else {
+                Toast.makeText(context, R.string.editor_toast_does_not_select, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+        super.onStop();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onRefreshEvent(Boolean refresh) {
+        if (refresh) {
+            EventBus.getDefault().post(new ContentChangedEvent(contentInput.getText().toString()));
+        }
     }
 }
