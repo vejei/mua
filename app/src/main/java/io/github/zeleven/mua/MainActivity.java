a9f509657c0c778b01579c97e4af0f41ea76abde
zeleven <chengsnem@163.com>
2018-01-25 17:24:58 +0800

    Setting language while activity create

--- app/src/main/java/io/github/zeleven/mua/MainActivity.java
+++ app/src/main/java/io/github/zeleven/mua/MainActivity.java
@@ -2,26 +2,38 @@ package io.github.zeleven.mua;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.Locale;
 
 import butterknife.BindString;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindString(R.string.app_name) String appName;
+    private SharedPreferences sharedPref;
 
-    @BindString(R.string.app_name)
-    String appName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        // get default shared preferences
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Setting language by shared preferences
+        settingLanguage();
 
         // open file list fragment
         getSupportFragmentManager().beginTransaction().replace(
@@ -65,5 +77,25 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void settingLanguage() {
+        Resources res = getResources();
+        Configuration cfg = res.getConfiguration();
+        DisplayMetrics metrics = res.getDisplayMetrics();
+        String value = sharedPref.getString("language", "");
+        Locale locale;
+        if (value.equals("device")) {
+            locale = Locale.getDefault();
+        } else {
+            if (value.contains("_")) {
+                String[] parts = value.split("_");
+                locale = new Locale(parts[0], parts[1]);
+            } else {
+                locale = new Locale(value);
+            }
+        }
+        cfg.setLocale(locale);
+        res.updateConfiguration(cfg, metrics);
     }
 }
