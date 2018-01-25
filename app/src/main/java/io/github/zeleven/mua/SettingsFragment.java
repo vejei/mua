8d0dc08fe74de588f8fa8ad7b6af5c231f9b61b5
zeleven <chengsnem@163.com>
2018-01-25 17:28:59 +0800

    completed settings

--- app/src/main/java/io/github/zeleven/mua/SettingsFragment.java
+++ app/src/main/java/io/github/zeleven/mua/SettingsFragment.java
@@ -1,11 +1,16 @@
 package io.github.zeleven.mua;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 
 import butterknife.BindString;
@@ -28,24 +33,116 @@ public class SettingsFragment extends BaseFragment {
     }
 
     public static class PreferenceFragmentCustom extends PreferenceFragmentCompat {
+        private AppCompatActivity context;
+        private SharedPreferences sharedPref;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             addPreferencesFromResource(R.xml.preferences);
+            context = (AppCompatActivity) getContext();
+            sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+            settingPreferences();
+        }
 
-            final AppCompatActivity context = (AppCompatActivity) getContext();
+        /**
+         * Preference item setting
+         */
+        public void settingPreferences() {
+            final SharedPreferences.Editor editor = sharedPref.edit();
 
-            // open AboutFragment on preference click
-            Preference drawerHeaderPreference = findPreference("drawer_header");
-            drawerHeaderPreference.setOnPreferenceClickListener(
-                    new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    return true;
+            // language setting
+            final ListPreference languagePref = (ListPreference) findPreference("language");
+            if (languagePref != null) {
+                final CharSequence[] entries = languagePref.getEntries();
+                String value = sharedPref.getString("language", "");
+                int index = languagePref.findIndexOfValue(value);
+                languagePref.setSummary(entries[index]);
+                languagePref.setOnPreferenceChangeListener(
+                        new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        editor.putString("language", (String) newValue).commit();
+                        languagePref.setSummary(entries[languagePref.findIndexOfValue(
+                                (String) newValue)]);
+                        Intent intent = new Intent(context, MainActivity.class);
+                        context.finish();
+                        startActivity(intent);
+                        return true;
+                    }
+                });
+            }
+
+            // drawer header image and text setting
+//            Preference drawerHeaderPref = findPreference("drawer_header");
+//            drawerHeaderPref.setOnPreferenceClickListener(
+//                    new Preference.OnPreferenceClickListener() {
+//                @Override
+//                public boolean onPreferenceClick(Preference preference) {
+//                    return true;
+//                }
+//            });
+
+            // file extension setting
+            final ListPreference fileExtPref = (ListPreference) findPreference("file_extension");
+            if (fileExtPref != null) {
+                int index = fileExtPref.findIndexOfValue(sharedPref.getString("file_extension", ""));
+                final CharSequence[] entries = fileExtPref.getEntries();
+                fileExtPref.setSummary(entries[index]);
+                fileExtPref.setOnPreferenceChangeListener(
+                        new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        editor.putString("file_extension", (String) newValue).commit();
+                        int newIndex = fileExtPref.findIndexOfValue((String) newValue);
+                        fileExtPref.setSummary(entries[newIndex]);
+                        return true;
+                    }
+                });
+            }
+
+            // sync options setting
+            final ListPreference syncPref = (ListPreference) findPreference("sync");
+            if (syncPref != null) {
+                int index = syncPref.findIndexOfValue(sharedPref.getString("sync", ""));
+                final CharSequence[] entries = syncPref.getEntries();
+                syncPref.setSummary(entries[index]);
+                syncPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        editor.putString("sync", (String) newValue).commit();
+                        int newIndex = syncPref.findIndexOfValue((String) newValue);
+                        syncPref.setSummary(entries[newIndex]);
+                        return true;
+                    }
+                });
+            }
+
+            // network setting
+            final CheckBoxPreference networkPref = (CheckBoxPreference) findPreference("network");
+            if (networkPref != null) {
+                boolean isChecked = sharedPref.getBoolean("network", true);
+                networkPref.setChecked(isChecked);
+                if (isChecked) {
+                    networkPref.setSummary(R.string.pref_summary_wifi_sync);
+                } else {
+                    networkPref.setSummary(R.string.pref_summary_4g_sync);
                 }
-            });
+                networkPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        editor.putBoolean("network", (Boolean) newValue).commit();
+                        networkPref.setChecked((Boolean) newValue);
+                        if ((Boolean) newValue) {
+                            networkPref.setSummary(R.string.pref_summary_wifi_sync);
+                        } else {
+                            networkPref.setSummary(R.string.pref_summary_4g_sync);
+                        }
+                        return false;
+                    }
+                });
+            }
 
-            // get version name and version code
+            // update preference setting, setting version name and version code
             String versionName = "";
             int versionCode = 1;
             try {
@@ -58,14 +155,14 @@ public class SettingsFragment extends BaseFragment {
                 e.printStackTrace();
             }
             // set version name and version code for preference item's summary
-            Preference versionPreference = findPreference("check_update");
+            Preference versionPref = findPreference("check_update");
             String appVersionText = getResources().getString(R.string.app_version_text);
-            versionPreference.setSummary(appVersionText + " " + versionName
+            versionPref.setSummary(appVersionText + " " + versionName
                     + " ( " + versionCode + " ) ");
 
-            // open AboutFragment on preference click
-            Preference aboutPreference = findPreference("about");
-            aboutPreference.setOnPreferenceClickListener(
+            // About preference setting, open AboutFragment on preference click
+            Preference aboutPref = findPreference("about");
+            aboutPref.setOnPreferenceClickListener(
                     new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
