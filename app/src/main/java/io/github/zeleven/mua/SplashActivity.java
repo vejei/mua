4c8ba57e88496ae788a9570c2acb31da5c72b6ea
zeleven <chengsnem@163.com>
2018-01-31 15:13:48 +0800

    Add splash screen

---
+++ app/src/main/java/io/github/zeleven/mua/SplashActivity.java
@@ -0,0 +1,34 @@
+package io.github.zeleven.mua;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+    @BindView(R.id.splash_layout) RelativeLayout splashLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        AlphaAnimation animation = new AlphaAnimation(0.0f , 1.0f ) ;
+        animation.setFillAfter(true);
+        animation.setDuration(1000);
+        splashLayout.startAnimation(animation);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+}
