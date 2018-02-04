eee081bd7bc5dc7c0bd2b5704a0216e4624e00af
zeleven <chengsnem@163.com>
2018-02-04 15:42:54 +0800

    update

--- app/src/main/java/io/github/zeleven/mua/MainActivity.java
+++ app/src/main/java/io/github/zeleven/mua/MainActivity.java
@@ -1,19 +1,13 @@
 package io.github.zeleven.mua;
 
-import android.app.SearchManager;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.widget.Toast;
 
-import java.io.File;
 import java.util.Locale;
 
 import butterknife.BindString;
@@ -38,39 +32,6 @@ public class MainActivity extends AppCompatActivity {
         // open file list fragment
         getSupportFragmentManager().beginTransaction().replace(
                 R.id.fragment_container, new FileListFragment()).commit();
-        // handle search box input
-        handelIntent(getIntent());
-        // create folder to save files
-        createFolder();
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handelIntent(intent);
-        super.onNewIntent(intent);
-    }
-
-    private void handelIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Toast.makeText(this, "关键词：" + query, Toast.LENGTH_SHORT);
-            //use the query to search your data somehow
-        }
-    }
-
-    /**
-     * Create folder to save files. If folder doesn't exist create it and log message, otherwise,
-     * just log the message.
-     */
-    private void createFolder() {
-        File folder = new File(Environment.getExternalStorageDirectory()
-                .getAbsolutePath() + "/" + appName);
-        if(!folder.exists()) {
-            folder.mkdir();
-            Log.i(getClass().getName(), "create success");
-        } else {
-            Log.i(getClass().getName(), "folder already exists");
-        }
     }
 
     @Override
@@ -85,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics metrics = res.getDisplayMetrics();
         String value = sharedPref.getString("language", "");
         Locale locale;
-        if (value.equals("device")) {
+        if (value.equals("auto")) {
             locale = Locale.getDefault();
         } else {
             if (value.contains("_")) {
