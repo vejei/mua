881cce54ccf8097787a86c2defe3150a6fa5606b
zeleven <chengsnem@163.com>
2018-02-04 17:06:07 +0800

    Add constants

--- app/src/main/java/io/github/zeleven/mua/Constants.java
+++ app/src/main/java/io/github/zeleven/mua/Constants.java
@@ -1,10 +1,10 @@
 package io.github.zeleven.mua;
 
 public class Constants {
-    public static final String MY_EMAIL = "";
+    public static final String MY_EMAIL = "chengsmo@outlook.com";
     public static final String MY_GITHUB = "http://github.com/zeleven";
     public static final String MY_ZHIHU = "https://www.zhihu.com/people/zheng-zheng-shu-36";
-    public static final String PROJECT_PAGE_URL = "";
+    public static final String PROJECT_PAGE_URL = "https://github.com/zeleven/mua";
 
     public static final String BUNDLE_KEY_FROM_FILE = "FROM_FILE";
     public static final String BUNDLE_KEY_SAVED = "SAVED";
